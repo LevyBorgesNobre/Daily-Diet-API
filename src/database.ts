5ed272488747024fae82ab81de8d26931a787918
Levy Borges <levyborges007@gmail.com>
2025-10-02 19:39:33 -0300

    chore(config): validate env with Zod, setup Knex, and create initial migration

---
+++ src/database.ts
@@ -0,0 +1,16 @@
+import knex from 'knex';
+import { env } from './env';
+
+export const config = {
+    client: 'sqlite',
+    connection: {
+        filename: env.DATABASE_URL ,
+    },
+    useNullAsDefault: true,
+    migrations:{
+        extension: 'ts',
+        directory: './db/migrations'
+    }
+}
+
+export const knexSetup = knex(config)
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import {app} from './app'
+import { env } from './env'
 
-app.listen({port: 3333}).then(()=>{
-    console.log('HTTP server running on http://localhost:3333')
+app.listen({port:env.PORT}).then(()=>{
+    console.log(`HTTP server running on ${env.PORT}`)
 })
