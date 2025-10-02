48e3d7e3d4a398352d47022422a727f9b3b4ad41
Levy Borges <levyborges007@gmail.com>
2025-10-02 20:07:42 -0300

    feat(db): create meals table and add session_id column

--- db/migrations/20251002222900_meals-log.ts
+++ db/migrations/20251002222900_meals-log.ts
@@ -1,9 +1,18 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('meals', (table)=>{
+     table.uuid('id').primary()
+     table.text('name').notNullable()
+     table.text('description').notNullable()
+     table.timestamp('created-at').defaultTo(knex.fn.now()).notNullable()
+     
+    })
+
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('meals')
 }
 
